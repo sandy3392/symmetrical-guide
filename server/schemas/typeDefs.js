bae0fb1964bf89f2ca83856e06a57227c756a19e
Sandeep Vittala <applesandy33@gmail.com>
2022-04-10 01:54:14 -0400

    finished typedefs and queries and server.js setup

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    type Book {
+        bookId: String
+        authors: [String]
+        description : String
+        title: String
+        image: String
+        link: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
+`;
+// export the typeDefs
+module.exports = typeDefs;
+
+
+
+
+
+// import the gql tagged template function
+// const { gql } = require('apollo-server-express');
+
+// const typeDefs = gql`
+//   type Query {
+//     helloWorld: String
+//   }
+// `;
+
+// export the typeDefs
+// module.exports = typeDefs;
